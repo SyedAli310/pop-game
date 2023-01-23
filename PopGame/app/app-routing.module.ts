6cecfa51b35c08a176dea159f6d863efeae0318d
Syed Ali <syed.a@keka.com>
2023-01-24 00:57:17 +0530

    added styles ans basic homepage components + added dependencies

--- PopGame/app/app-routing.module.ts
+++ PopGame/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomePageComponent } from './home-page';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: '', redirectTo: 'play', pathMatch: 'full' },
+      { path: 'play', component: HomePageComponent },
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
